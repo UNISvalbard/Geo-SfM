77fd85430325104f38a0344f0f1e52dd0e16b24d
Peter <28719452+PeterBetlem@users.noreply.github.com>
2024-01-23 12:26:36 +0100

    added model gallery

--- run.bat
+++ run.bat
@@ -2,6 +2,6 @@ set name=automated_metashape
 
 cd %~dp0
 CALL conda activate %name%
-CALL jupyter book build .
+CALL sphinx-build . ./_build/html -b html
 CALL ghp-import -n -p -f _build/html
 CALL conda env export -n %name% > %name%.yaml
